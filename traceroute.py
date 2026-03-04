0e7120c23761049ec5d2f7ea83385ccbb559a424
ssplinterot <margobolshakova49@gmail.com>
2026-03-04 22:32:42 +0300

    +comments(LK2)

--- traceroute.py
+++ traceroute.py
@@ -1,6 +1,7 @@
 # Запуск: sudo python3 traceroute.py google.com
 
 import socket, struct, time, sys
+# работа с сетью, числа в байты, замер времени, чтение аргументов в ком.строке
 
 def checksum(data):
     s = 0
@@ -9,42 +10,41 @@ def checksum(data):
     s = (s >> 16) + (s & 0xFFFF)
     return ~s & 0xFFFF
 
-def ping(dst, ttl, seq):
-    # Собираем ICMP Echo Request пакет (8 байт)
-    pkt = struct.pack("!BBHHH", 8, 0, 0, 1, seq) # (тип, код, временная контрольная сумма, идентификатор пакета, порядковый номер)
+def ping(dst, ttl, seq): # IP назначения, значение TTL, порядковый номер пакета.
+    # Echo Request  (8 байт)
+    pkt = struct.pack("!BBHHH", 8, 0, 0, 1, seq) # (big-endian, тип, код, временная контрольная сумма, id пакета, порядковый номер)
     pkt = struct.pack("!BBHHH", 8, 0, checksum(pkt), 1, seq)  # пересобираем с реальной суммой
 
-    tx = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-    rx = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    tx = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)# отправка (IPv4, сокет, протокол ICMP)
+    rx = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)# приём
     rx.settimeout(2)
 
     tx.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
-    tx.sendto(pkt, (dst, 0))# отправка сокета
-    t = time.time()
+    tx.sendto(pkt, (dst, 0))# отправка пакета (адрес и порт)
+    t = time.time()#время отправки в сек.
 
     try:
         while True:
             raw, addr = rx.recvfrom(1024)
-            icmp = raw[(raw[0] & 0xF) * 4:]   # срезаем IP-заголовок
-            typ  = icmp[0]
+            icmp = raw[(raw[0] & 0xF) * 4:]   # младшие 4 бита первого байта, это длина IP-заголовка. * на 4 — получаем длину в байтах
+            typ  = icmp[0] #первый байт ICMP — тип пакета
 
-            # Тип 11 = TTL Exceeded (от роутера), тип 0 = Echo Reply (от цели)
             check_icmp = icmp[28:] if typ == 11 else icmp
             if struct.unpack("!H", check_icmp[6:8])[0] == seq:
-                return (time.time() - t) * 1000, addr[0]
+                return (time.time() - t) * 1000, addr[0]# время в миллисекундах и IP ответившего узла
     except socket.timeout:
         return None, None
     finally:
         tx.close(); rx.close()
 
-dst  = sys.argv[1]
-ip   = socket.gethostbyname(dst)
+dst  = sys.argv[1] # sys.argv — список аргументов из командной строки. [0] = имя файла, [1] = первый аргумент
+ip   = socket.gethostbyname(dst) # превращает в IP
 print(f"\ntraceroute to {dst} ({ip})\n")
 
 seq = 1
 for ttl in range(1, 31):
     results = []
-    for i in range(3):
+    for i in range(3): # 3 пакета на каждый хоп
        results.append(ping(ip, ttl, seq))
        seq += 1  
     hop_ip  = next((r[1] for r in results if r[1]), None)
